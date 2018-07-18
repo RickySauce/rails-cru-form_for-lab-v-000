b33a695e4b5957c5a703fba9dc88766543c116a6
Eric DeSimone <eric.desimone@cortland.edu>
2018-07-18 14:25:24 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -10,6 +10,7 @@ class SongsController < ApplicationController
 
   def create
     @song = Song.create(song_params(:name, :artist_id, :genre_id))
+    redirect_to song_path(@song)
     binding.pry
   end
 

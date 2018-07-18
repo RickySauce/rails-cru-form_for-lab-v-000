aa30c67de41d66d38fff34f6d077ed10e278fe22
Eric DeSimone <eric.desimone@cortland.edu>
2018-07-18 13:57:31 +0000

    automatically backed up by learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -9,6 +9,12 @@ class ArtistsController < ApplicationController
   end
 
   def create
+    @artist = Artist.create()
   end
 
+  private 
+
+  def artist_params(*args)
+    params.require(:artists).permit(*args)
+  end
 end

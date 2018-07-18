af164f917cb8c4d2a7fa1ee0823ccb19a8e683e8
Eric DeSimone <eric.desimone@cortland.edu>
2018-07-18 14:11:56 +0000

    automatically backed up by learn

--- app/controllers/concerns/songs_controller.rb
+++ app/controllers/concerns/songs_controller.rb
@@ -1,2 +1,23 @@
-class SongsController < ApplicationController 
+class SongsController < ApplicationController
+
+  def show 
+  end 
+
+  def new 
+  end 
+
+  def create 
+  end 
+
+  def edit 
+  end  
+
+  def update 
+  end 
+
+  private 
+
+  def song_params(*args) 
+    params.require(:song).permit(*args)
+  end
 end

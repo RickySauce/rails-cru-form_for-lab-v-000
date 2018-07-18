30213bc9201b4170dcaa4cdd4cf33deb53da969e
Eric DeSimone <eric.desimone@cortland.edu>
2018-07-18 14:08:48 +0000

    automatically backed up by learn

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -10,7 +10,7 @@ class GenresController < ApplicationController
 
   def create
     @genre = Genre.create(genre_params(:name))
-    redirect_to genre_path(@path)
+    redirect_to genre_path(@genre)
   end
 
   def edit
